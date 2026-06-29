d15c6794eb2486c930c7bc9b4e86cbe3da719e61
irisparadox <gasaav01@ucm.es>
2026-06-29 17:13:06 +0200

    Added clamp, min, max, redmin, redmax

--- linalg.h
+++ linalg.h
@@ -118,6 +118,66 @@ VEC_DECL vec2f_t vf2normalize_v(vec2f_t v);
 VEC_DECL vec3f_t vf3normalize_v(vec3f_t v);
 VEC_DECL vec4f_t vf4normalize_v(vec4f_t v);
 
+VEC_DECL vec2i_t v2clamp_vx(vec2i_t v, int lo, int hi);
+VEC_DECL vec3i_t v3clamp_vx(vec3i_t v, int lo, int hi);
+VEC_DECL vec4i_t v4clamp_vx(vec4i_t v, int lo, int hi);
+
+VEC_DECL vec2i_t v2clamp_vv(vec2i_t v, vec2i_t lo, vec2i_t hi);
+VEC_DECL vec3i_t v3clamp_vv(vec3i_t v, vec3i_t lo, vec3i_t hi);
+VEC_DECL vec4i_t v4clamp_vv(vec4i_t v, vec4i_t lo, vec4i_t hi);
+
+VEC_DECL vec2f_t vf2clamp_vf(vec2f_t v, float lo, float hi);
+VEC_DECL vec3f_t vf3clamp_vf(vec3f_t v, float lo, float hi);
+VEC_DECL vec4f_t vf4clamp_vf(vec4f_t v, float lo, float hi);
+
+VEC_DECL vec2f_t vf2clamp_vv(vec2f_t v, vec2f_t lo, vec2f_t hi);
+VEC_DECL vec3f_t vf3clamp_vv(vec3f_t v, vec3f_t lo, vec3f_t hi);
+VEC_DECL vec4f_t vf4clamp_vv(vec4f_t v, vec4f_t lo, vec4f_t hi);
+
+VEC_DECL vec2i_t v2min_vx(vec2i_t v, int x);
+VEC_DECL vec3i_t v3min_vx(vec3i_t v, int x);
+VEC_DECL vec4i_t v4min_vx(vec4i_t v, int x);
+VEC_DECL vec2i_t v2max_vx(vec2i_t v, int x);
+VEC_DECL vec3i_t v3max_vx(vec3i_t v, int x);
+VEC_DECL vec4i_t v4max_vx(vec4i_t v, int x);
+
+VEC_DECL vec2f_t vf2min_vf(vec2f_t v, float x);
+VEC_DECL vec3f_t vf3min_vf(vec3f_t v, float x);
+VEC_DECL vec4f_t vf4min_vf(vec4f_t v, float x);
+VEC_DECL vec2f_t vf2max_vf(vec2f_t v, float x);
+VEC_DECL vec3f_t vf3max_vf(vec3f_t v, float x);
+VEC_DECL vec4f_t vf4max_vf(vec4f_t v, float x);
+
+VEC_DECL vec2i_t v2min_vv(vec2i_t vs1, vec2i_t vs2);
+VEC_DECL vec3i_t v3min_vv(vec3i_t vs1, vec3i_t vs2);
+VEC_DECL vec4i_t v4min_vv(vec4i_t vs1, vec4i_t vs2);
+VEC_DECL vec2i_t v2max_vv(vec2i_t vs1, vec2i_t vs2);
+VEC_DECL vec3i_t v3max_vv(vec3i_t vs1, vec3i_t vs2);
+VEC_DECL vec4i_t v4max_vv(vec4i_t vs1, vec4i_t vs2);
+
+VEC_DECL vec2f_t vf2min_vv(vec2f_t vs1, vec2f_t vs2);
+VEC_DECL vec3f_t vf3min_vv(vec3f_t vs1, vec3f_t vs2);
+VEC_DECL vec4f_t vf4min_vv(vec4f_t vs1, vec4f_t vs2);
+VEC_DECL vec2f_t vf2max_vv(vec2f_t vs1, vec2f_t vs2);
+VEC_DECL vec3f_t vf3max_vv(vec3f_t vs1, vec3f_t vs2);
+VEC_DECL vec4f_t vf4max_vv(vec4f_t vs1, vec4f_t vs2);
+
+VEC_DECL int v2redmin_v(vec2i_t v);
+VEC_DECL int v3redmin_v(vec3i_t v);
+VEC_DECL int v4redmin_v(vec4i_t v);
+
+VEC_DECL int v2redmax_v(vec2i_t v);
+VEC_DECL int v3redmax_v(vec3i_t v);
+VEC_DECL int v4redmax_v(vec4i_t v);
+
+VEC_DECL float vf2redmin_v(vec2f_t v);
+VEC_DECL float vf3redmin_v(vec3f_t v);
+VEC_DECL float vf4redmin_v(vec4f_t v);
+
+VEC_DECL float vf2redmax_v(vec2f_t v);
+VEC_DECL float vf3redmax_v(vec3f_t v);
+VEC_DECL float vf4redmax_v(vec4f_t v);
+
 #endif // LINALG_H
 
 #ifdef LINALG_IMPLEMENTATION
@@ -137,6 +197,36 @@ static inline float i_linalg_rsqrtf(float x)
 	y = y * (1.5f - (x2 * y * y));
 	y = y * (1.5f - (x2 * y * y));
 	return y;
+}
+
+static inline int i_linalg_clampi(int v, int lo, int hi)
+{
+	return v < lo ? lo : v > hi ? hi : v;
+}
+
+static inline float i_linalg_clampf(float v, float lo, float hi)
+{
+	return v < lo ? lo : v > hi ? hi : v;
+}
+
+static inline int i_linalg_mini(int v1, int v2)
+{
+	return v2 + ((v1 - v2) & ((v1 - v2) >> 31));
+}
+
+static inline int i_linalg_maxi(int v1, int v2)
+{
+	return v1 - ((v1 - v2) & ((v1 - v2) >> 31));
+}
+
+static inline float i_linalg_minf(float v1, float v2)
+{
+	return v1 < v2 ? v1 : v2;
+}
+
+static inline float i_linalg_maxf(float v1, float v2)
+{
+	return v1 > v2 ? v1 : v2;
 }
 
 // -------------------------------------------------
@@ -506,6 +596,315 @@ VEC_DECL vec4f_t vf4normalize_v(vec4f_t v)
 			  .y = v.y * rlen2,
 			  .z = v.z * rlen2,
 			  .w = v.w * rlen2 };
+}
+
+VEC_DECL vec2i_t v2clamp_vx(vec2i_t v, int lo, int hi)
+{
+	return (vec2i_t){ .x = i_linalg_clampi(v.x, lo, hi),
+			  .y = i_linalg_clampi(v.y, lo, hi) };
+}
+
+VEC_DECL vec3i_t v3clamp_vx(vec3i_t v, int lo, int hi)
+{
+	return (vec3i_t){ .x = i_linalg_clampi(v.x, lo, hi),
+			  .y = i_linalg_clampi(v.y, lo, hi),
+			  .z = i_linalg_clampi(v.z, lo, hi) };
+}
+
+VEC_DECL vec4i_t v4clamp_vx(vec4i_t v, int lo, int hi)
+{
+	return (vec4i_t){ .x = i_linalg_clampi(v.x, lo, hi),
+			  .y = i_linalg_clampi(v.y, lo, hi),
+			  .z = i_linalg_clampi(v.z, lo, hi),
+			  .w = i_linalg_clampi(v.w, lo, hi) };
+}
+
+VEC_DECL vec2i_t v2clamp_vv(vec2i_t v, vec2i_t lo, vec2i_t hi)
+{
+	return (vec2i_t){ .x = i_linalg_clampi(v.x, lo.x, hi.x),
+			  .y = i_linalg_clampi(v.y, lo.y, hi.y) };
+}
+
+VEC_DECL vec3i_t v3clamp_vv(vec3i_t v, vec3i_t lo, vec3i_t hi)
+{
+	return (vec3i_t){ .x = i_linalg_clampi(v.x, lo.x, hi.x),
+			  .y = i_linalg_clampi(v.y, lo.y, hi.y),
+			  .z = i_linalg_clampi(v.z, lo.z, hi.z) };
+}
+
+VEC_DECL vec4i_t v4clamp_vv(vec4i_t v, vec4i_t lo, vec4i_t hi)
+{
+	return (vec4i_t){ .x = i_linalg_clampi(v.x, lo.x, hi.x),
+			  .y = i_linalg_clampi(v.y, lo.y, hi.y),
+			  .z = i_linalg_clampi(v.z, lo.z, hi.z),
+			  .w = i_linalg_clampi(v.w, lo.w, hi.w) };
+}
+
+VEC_DECL vec2f_t vf2clamp_vf(vec2f_t v, float lo, float hi)
+{
+	return (vec2f_t){ .x = i_linalg_clampf(v.x, lo, hi),
+			  .y = i_linalg_clampf(v.y, lo, hi) };
+}
+
+VEC_DECL vec3f_t vf3clamp_vf(vec3f_t v, float lo, float hi)
+{
+	return (vec3f_t){ .x = i_linalg_clampf(v.x, lo, hi),
+			  .y = i_linalg_clampf(v.y, lo, hi),
+			  .z = i_linalg_clampf(v.z, lo, hi) };
+}
+
+VEC_DECL vec4f_t vf4clamp_vf(vec4f_t v, float lo, float hi)
+{
+	return (vec4f_t){ .x = i_linalg_clampf(v.x, lo, hi),
+			  .y = i_linalg_clampf(v.y, lo, hi),
+			  .z = i_linalg_clampf(v.z, lo, hi),
+			  .w = i_linalg_clampf(v.w, lo, hi) };
+}
+
+VEC_DECL vec2f_t vf2clamp_vv(vec2f_t v, vec2f_t lo, vec2f_t hi)
+{
+	return (vec2f_t){ .x = i_linalg_clampf(v.x, lo.x, hi.x),
+			  .y = i_linalg_clampf(v.y, lo.y, hi.y) };
+}
+
+VEC_DECL vec3f_t vf3clamp_vv(vec3f_t v, vec3f_t lo, vec3f_t hi)
+{
+	return (vec3f_t){ .x = i_linalg_clampf(v.x, lo.x, hi.x),
+			  .y = i_linalg_clampf(v.y, lo.y, hi.y),
+			  .z = i_linalg_clampf(v.z, lo.z, hi.z) };
+}
+
+VEC_DECL vec4f_t vf4clamp_vv(vec4f_t v, vec4f_t lo, vec4f_t hi)
+{
+	return (vec4f_t){ .x = i_linalg_clampf(v.x, lo.x, hi.x),
+			  .y = i_linalg_clampf(v.y, lo.y, hi.y),
+			  .z = i_linalg_clampf(v.z, lo.z, hi.z),
+			  .w = i_linalg_clampf(v.w, lo.w, hi.w) };
+}
+
+VEC_DECL vec2i_t v2min_vx(vec2i_t v, int x)
+{
+	return (vec2i_t){ .x = i_linalg_mini(v.x, x), .y = i_linalg_mini(v.y, x) };
+}
+
+VEC_DECL vec3i_t v3min_vx(vec3i_t v, int x)
+{
+	return (vec3i_t){ .x = i_linalg_mini(v.x, x),
+			  .y = i_linalg_mini(v.y, x),
+			  .z = i_linalg_mini(v.z, x) };
+}
+
+VEC_DECL vec4i_t v4min_vx(vec4i_t v, int x)
+{
+	return (vec4i_t){ .x = i_linalg_mini(v.x, x),
+			  .y = i_linalg_mini(v.y, x),
+			  .z = i_linalg_mini(v.z, x),
+			  .w = i_linalg_mini(v.w, x) };
+}
+
+VEC_DECL vec2i_t v2max_vx(vec2i_t v, int x)
+{
+	return (vec2i_t){ .x = i_linalg_maxi(v.x, x), .y = i_linalg_maxi(v.y, x) };
+}
+
+VEC_DECL vec3i_t v3max_vx(vec3i_t v, int x)
+{
+	return (vec3i_t){ .x = i_linalg_maxi(v.x, x),
+			  .y = i_linalg_maxi(v.y, x),
+			  .z = i_linalg_maxi(v.z, x) };
+}
+
+VEC_DECL vec4i_t v4max_vx(vec4i_t v, int x)
+{
+	return (vec4i_t){ .x = i_linalg_maxi(v.x, x),
+			  .y = i_linalg_maxi(v.y, x),
+			  .z = i_linalg_maxi(v.z, x),
+			  .w = i_linalg_maxi(v.w, x) };
+}
+
+VEC_DECL vec2f_t vf2min_vf(vec2f_t v, float x)
+{
+	return (vec2f_t){ .x = i_linalg_minf(v.x, x), .y = i_linalg_minf(v.y, x) };
+}
+
+VEC_DECL vec3f_t vf3min_vf(vec3f_t v, float x)
+{
+	return (vec3f_t){ .x = i_linalg_minf(v.x, x),
+			  .y = i_linalg_minf(v.y, x),
+			  .z = i_linalg_minf(v.z, x) };
+}
+
+VEC_DECL vec4f_t vf4min_vf(vec4f_t v, float x)
+{
+	return (vec4f_t){ .x = i_linalg_minf(v.x, x),
+			  .y = i_linalg_minf(v.y, x),
+			  .z = i_linalg_minf(v.z, x),
+			  .w = i_linalg_minf(v.w, x) };
+}
+
+VEC_DECL vec2f_t vf2max_vf(vec2f_t v, float x)
+{
+	return (vec2f_t){ .x = i_linalg_maxf(v.x, x), .y = i_linalg_maxf(v.y, x) };
+}
+
+VEC_DECL vec3f_t vf3max_vf(vec3f_t v, float x)
+{
+	return (vec3f_t){ .x = i_linalg_maxf(v.x, x),
+			  .y = i_linalg_maxf(v.y, x),
+			  .z = i_linalg_maxf(v.z, x) };
+}
+
+VEC_DECL vec4f_t vf4max_vf(vec4f_t v, float x)
+{
+	return (vec4f_t){ .x = i_linalg_maxf(v.x, x),
+			  .y = i_linalg_maxf(v.y, x),
+			  .z = i_linalg_maxf(v.z, x),
+			  .w = i_linalg_maxf(v.w, x) };
+}
+
+VEC_DECL vec2i_t v2min_vv(vec2i_t vs1, vec2i_t vs2)
+{
+	return (vec2i_t){ .x = i_linalg_mini(vs1.x, vs2.x),
+			  .y = i_linalg_mini(vs1.y, vs2.y) };
+}
+
+VEC_DECL vec3i_t v3min_vv(vec3i_t vs1, vec3i_t vs2)
+{
+	return (vec3i_t){ .x = i_linalg_mini(vs1.x, vs2.x),
+			  .y = i_linalg_mini(vs1.y, vs2.y),
+			  .z = i_linalg_mini(vs1.z, vs2.z) };
+}
+
+VEC_DECL vec4i_t v4min_vv(vec4i_t vs1, vec4i_t vs2)
+{
+	return (vec4i_t){ .x = i_linalg_mini(vs1.x, vs2.x),
+			  .y = i_linalg_mini(vs1.y, vs2.y),
+			  .z = i_linalg_mini(vs1.z, vs2.z),
+			  .w = i_linalg_mini(vs1.w, vs2.w) };
+}
+
+VEC_DECL vec2i_t v2max_vv(vec2i_t vs1, vec2i_t vs2)
+{
+	return (vec2i_t){ .x = i_linalg_maxi(vs1.x, vs2.x),
+			  .y = i_linalg_maxi(vs1.y, vs2.y) };
+}
+
+VEC_DECL vec3i_t v3max_vv(vec3i_t vs1, vec3i_t vs2)
+{
+	return (vec3i_t){ .x = i_linalg_maxi(vs1.x, vs2.x),
+			  .y = i_linalg_maxi(vs1.y, vs2.y),
+			  .z = i_linalg_maxi(vs1.z, vs2.z) };
+}
+
+VEC_DECL vec4i_t v4max_vv(vec4i_t vs1, vec4i_t vs2)
+{
+	return (vec4i_t){ .x = i_linalg_maxi(vs1.x, vs2.x),
+			  .y = i_linalg_maxi(vs1.y, vs2.y),
+			  .z = i_linalg_maxi(vs1.z, vs2.z),
+			  .w = i_linalg_maxi(vs1.w, vs2.w) };
+}
+
+VEC_DECL vec2f_t vf2min_vv(vec2f_t vs1, vec2f_t vs2)
+{
+	return (vec2f_t){ .x = i_linalg_minf(vs1.x, vs2.x),
+			  .y = i_linalg_minf(vs1.y, vs2.y) };
+}
+
+VEC_DECL vec3f_t vf3min_vv(vec3f_t vs1, vec3f_t vs2)
+{
+	return (vec3f_t){ .x = i_linalg_minf(vs1.x, vs2.x),
+			  .y = i_linalg_minf(vs1.y, vs2.y),
+			  .z = i_linalg_minf(vs1.z, vs2.z) };
+}
+
+VEC_DECL vec4f_t vf4min_vv(vec4f_t vs1, vec4f_t vs2)
+{
+	return (vec4f_t){ .x = i_linalg_minf(vs1.x, vs2.x),
+			  .y = i_linalg_minf(vs1.y, vs2.y),
+			  .z = i_linalg_minf(vs1.z, vs2.z),
+			  .w = i_linalg_minf(vs1.w, vs2.w) };
+}
+
+VEC_DECL vec2f_t vf2max_vv(vec2f_t vs1, vec2f_t vs2)
+{
+	return (vec2f_t){ .x = i_linalg_maxf(vs1.x, vs2.x),
+			  .y = i_linalg_maxf(vs1.y, vs2.y) };
+}
+
+VEC_DECL vec3f_t vf3max_vv(vec3f_t vs1, vec3f_t vs2)
+{
+	return (vec3f_t){ .x = i_linalg_maxf(vs1.x, vs2.x),
+			  .y = i_linalg_maxf(vs1.y, vs2.y),
+			  .z = i_linalg_maxf(vs1.z, vs2.z) };
+}
+
+VEC_DECL vec4f_t vf4max_vv(vec4f_t vs1, vec4f_t vs2)
+{
+	return (vec4f_t){ .x = i_linalg_maxf(vs1.x, vs2.x),
+			  .y = i_linalg_maxf(vs1.y, vs2.y),
+			  .z = i_linalg_maxf(vs1.z, vs2.z),
+			  .w = i_linalg_maxf(vs1.w, vs2.w) };
+}
+
+VEC_DECL int v2redmin_v(vec2i_t v)
+{
+	return i_linalg_mini(v.x, v.y);
+}
+
+VEC_DECL int v3redmin_v(vec3i_t v)
+{
+	return i_linalg_mini(v.x, i_linalg_mini(v.y, v.z));
+}
+
+VEC_DECL int v4redmin_v(vec4i_t v)
+{
+	return i_linalg_mini(i_linalg_mini(v.x, v.y), i_linalg_mini(v.z, v.w));
+}
+
+VEC_DECL int v2redmax_v(vec2i_t v)
+{
+	return i_linalg_maxi(v.x, v.y);
+}
+
+VEC_DECL int v3redmax_v(vec3i_t v)
+{
+	return i_linalg_maxi(v.x, i_linalg_maxi(v.y, v.z));
+}
+
+VEC_DECL int v4redmax_v(vec4i_t v)
+{
+	return i_linalg_maxi(i_linalg_maxi(v.x, v.y), i_linalg_maxi(v.z, v.w));
+}
+
+
+VEC_DECL float vf2redmin_v(vec2f_t v)
+{
+	return i_linalg_minf(v.x, v.y);
+}
+
+VEC_DECL float vf3redmin_v(vec3f_t v)
+{
+	return i_linalg_minf(v.x, i_linalg_minf(v.y, v.z));
+}
+
+VEC_DECL float vf4redmin_v(vec4f_t v)
+{
+	return i_linalg_minf(i_linalg_minf(v.x, v.y), i_linalg_minf(v.z, v.w));
+}
+
+VEC_DECL float vf2redmax_v(vec2f_t v)
+{
+	return i_linalg_maxf(v.x, v.y);
+}
+
+VEC_DECL float vf3redmax_v(vec3f_t v)
+{
+	return i_linalg_maxf(v.x, i_linalg_maxf(v.y, v.z));
+}
+
+VEC_DECL float vf4redmax_v(vec4f_t v)
+{
+	return i_linalg_maxf(i_linalg_maxf(v.x, v.y), i_linalg_maxf(v.z, v.w));
 }
 
 #endif // I_LINALG_IMPLEMENTATION
